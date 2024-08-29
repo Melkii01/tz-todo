0ef7f31321a2eec1dd95024d8e3d2ef9b8f979aa
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-08-29 17:47:21 +0300

    fix(component): оптимизация todo-footer

--- projects/lib-todo/src/lib/todo/components/todo-footer/todo-footer.component.ts
+++ projects/lib-todo/src/lib/todo/components/todo-footer/todo-footer.component.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {BehaviorSubject, Subject, takeUntil, tap} from "rxjs";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {Observable, tap} from "rxjs";
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FilterNames} from "../../types/filter-names";
 
@@ -9,30 +9,20 @@ import {FilterNames} from "../../types/filter-names";
   styleUrls: ['./todo-footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TodoFooterComponent implements OnInit, OnDestroy {
+export class TodoFooterComponent {
   @Input() count: number | null = 0;
   @Input() checkedAtLeastOne: boolean | null = false;
   @Output() clearedCompletedEvent: EventEmitter<string> = new EventEmitter<string>();
   filterParam: string = '';
+  filterParam$: Observable<Params> = this.activatedRoute.queryParams.pipe(
+    tap((params: Params): void => {
+      this.filterParam = params[FilterNames.filter];
+    })
+  );
   protected readonly FilterNames = FilterNames;
-  private destroy$: Subject<void> = new Subject<void>()
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    this.init();
-  }
-
-  init(): void {
-    this.activatedRoute.queryParams.pipe(
-      tap((params: Params): void => {
-        this.filterParam = params[FilterNames.filter];
-      }),
-
-      takeUntil(this.destroy$)
-    ).subscribe();
   }
 
   /**
@@ -44,7 +34,7 @@ export class TodoFooterComponent implements OnInit, OnDestroy {
 
   /**
    * Меняет фильтр параметры согласно запросам
-   * @param filter параметры фильтра
+   * @param filter параметры фильтра url
    */
   choosingFilterParam(filter: string): void {
     this.router.navigate([], {
@@ -52,10 +42,5 @@ export class TodoFooterComponent implements OnInit, OnDestroy {
       queryParams: {filter},
       queryParamsHandling: 'merge'
     });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
